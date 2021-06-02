dcbc87a0a07d9942956726681b1ac56809584319
Reugito <65726495+Reugito@users.noreply.github.com>
2021-06-02 19:09:13 +0530

    Show if it's win or tie between heads and tails

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -11,11 +11,12 @@ do
    else
       ((tailswin++))
    fi
+   if [ $headswin -eq $tailswin ]
+   then
+      echo "It's a tie "
+      break
+   fi
 done
-if [ $headswin -eq $tailswin ]
-then
-   echo "It's a tie "
-fi
 echo "Heads win $headswin times"
 echo "Tails win $tailswin times"
 
