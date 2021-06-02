da6c2ce97d567866a0f006fc668b547b8da37b5b
Reugito <[email>
2021-06-02 19:05:54 +0530

    if ties loop over while diif of win lessthan 2

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -11,11 +11,23 @@ do
    else
       ((tailswin++))
    fi
+   if [ $headswin -eq $tailswin ]
+   then
+      echo "It's a tie "
+      while [ $(($headswin-$tailswin)) -le 2 ]
+      do
+         toss=$((RANDOM%2))
+         if [ $toss -eq $heads ]
+         then
+            ((headswin++))
+         else
+            ((tailswin++))
+         fi
+      done
+      break
+      fi
+
 done
-if [ $headswin -eq $tailswin ]
-then
-   echo "It's a tie "
-fi
 echo "Heads win $headswin times"
 echo "Tails win $tailswin times"
 
